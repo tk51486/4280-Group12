76aedb6cbfec0824f31d3d21ffb97ce377c79b62
aam08331 <112673088+aam08331@users.noreply.github.com>
2025-04-09 19:07:51 -0400

    added sd to ddr2 project

---
+++ LRU/DDR2 Example/mig_example-master/sdCode/fpga_top.v
@@ -0,0 +1,108 @@
+
+//--------------------------------------------------------------------------------------------------------
+// Module  : fpga_top
+// Type    : synthesizable, FPGA's top, IP's example design
+// Standard: Verilog 2001 (IEEE1364-2001)
+// Function: an example of sd_file_reader, read a file from SDcard and send file content to UART
+//           this example runs on Digilent Nexys4-DDR board (Xilinx Artix-7),
+//           see http://www.digilent.com.cn/products/product-nexys-4-ddr-artix-7-fpga-trainer-board.html
+//--------------------------------------------------------------------------------------------------------
+
+module fpga_top (
+    // clock = 100MHz
+    input  wire         clk100mhz,
+    // rstn active-low, You can re-read SDcard by pushing the reset button.
+    input  wire         resetn,
+    // when sdcard_pwr_n = 0, SDcard power on
+    output wire         sdcard_pwr_n,
+    // signals connect to SD bus
+    output wire         sdclk,
+    inout               sdcmd,
+    input  wire         sddat0,
+    output wire         sddat1, sddat2, sddat3,
+    // 16 bit led to show the status of SDcard
+    output wire [15:0]  led,
+    // UART tx signal, connected to host-PC's UART-RXD, baud=115200
+    output wire         uart_tx
+);
+
+
+
+assign sdcard_pwr_n = 1'b0;                  // keep SDcard power-on
+
+assign {sddat1, sddat2, sddat3} = 3'b111;    // Must set sddat1~3 to 1 to avoid SD card from entering SPI mode
+
+
+//assign led[15:11] = 1;
+//----------------------------------------------------------------------------------------------------
+// generate 50MHz clk from 100MHz clk
+//----------------------------------------------------------------------------------------------------
+wire       clk;
+wire       rstn;
+
+clk_wiz_0 u_clk_wiz_0 (
+    .resetn      ( resetn       ),
+    .clk_in1     ( clk100mhz    ),
+    .locked      ( rstn         ),
+    .clk_out1    ( clk          )        // 50MHz
+);
+
+
+
+//----------------------------------------------------------------------------------------------------
+// sd_file_reader
+//----------------------------------------------------------------------------------------------------
+wire       outen;     // when outen=1, a byte of file content is read out from outbyte
+wire [7:0] outbyte;   // a byte of file content
+
+sd_file_reader #(
+    .FILE_NAME_LEN    ( 11             ),  // the length of "example.txt" (in bytes)
+    .FILE_NAME        ( "example.txt"  ),  // file name to read
+    .CLK_DIV          ( 2              )   // because clk=50MHz, CLK_DIV must ≥2
+) u_sd_file_reader (
+    .rstn             ( rstn           ),
+    .clk              ( clk            ),
+    .sdclk            ( sdclk          ),
+    .sdcmd            ( sdcmd          ),
+    .sddat0           ( sddat0         ),
+    .card_stat        ( led[3:0]       ),  // show the sdcard initialize status
+    .card_type        ( led[5:4]       ),  // 0=UNKNOWN    , 1=SDv1    , 2=SDv2  , 3=SDHCv2
+    .filesystem_type  ( led[7:6]       ),  // 0=UNASSIGNED , 1=UNKNOWN , 2=FAT16 , 3=FAT32 
+    .file_found       ( led[  8]       ),  // 0=file not found, 1=file found
+    .outen            ( outen          ),
+    .outbyte          ( outbyte        )
+);
+
+//assign led[15:11] = outbyte[3:0];
+
+//----------------------------------------------------------------------------------------------------
+// send file content to UART
+//----------------------------------------------------------------------------------------------------
+/*uart_tx #(
+    .CLK_FREQ                  ( 50000000             ),    // clk is 50MHz
+    .BAUD_RATE                 ( 115200               ),
+    .PARITY                    ( "NONE"               ),
+    .STOP_BITS                 ( 2                    ),
+    .BYTE_WIDTH                ( 1                    ),
+    .FIFO_EA                   ( 18                   ),
+    .EXTRA_BYTE_AFTER_TRANSFER ( ""                   ),
+    .EXTRA_BYTE_AFTER_PACKET   ( ""                   )
+) u_uart_tx (
+    .rstn                      ( rstn                 ),
+    .clk                       ( clk                  ),
+    .i_tready                  (                      ),
+    .i_tvalid                  ( outen                ),
+    .i_tdata                   ( outbyte              ),
+    .i_tkeep                   ( 1'b1                 ),
+    .i_tlast                   ( 1'b0                 ),
+    .o_uart_tx                 ( uart_tx              )
+);
+*/
+sd_test(
+    .currentNum(outbyte),
+    .clk(clk),
+    .led(led[15:9])
+);
+
+
+endmodule
---
+++ LRU/DDR2 Example/mig_example-master/sdCode/sd_test.v
@@ -0,0 +1,103 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/06/2025 01:34:18 PM
+// Design Name: 
+// Module Name: sd_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sd_test(
+        input wire [7:0] currentNum,
+        input wire clk,
+        output wire [15:9] led
+    );
+reg [7:0] testVals [8:0];
+reg [7:0] buff [0:50000];
+reg [32:0] bufco;
+reg [32:0] finalco;
+reg [38:0] counter;
+reg [16:0] nintcount;
+reg [3:0] extra;
+reg test;
+reg ter;
+reg on;
+reg [6:0] inc;
+reg [7:0] check;
+reg [16:0] wai;
+initial begin
+    testVals[0] = 8'b00000001;
+    testVals[1] = 8'b00000010;
+    testVals[2] = 8'b00000011;
+    testVals[3] = 8'b00000100;
+    testVals[4] = 8'b00000101;
+    testVals[5] = 8'b00000110;
+    testVals[6] = 8'b00000111;
+    testVals[7] = 8'b00001000;
+    testVals[8] = 8'b00001010;
+    counter = 0;
+    bufco = 0;
+    finalco = 49000;
+    extra = 4'b0000;
+    test = 1;
+    ter = 0;
+    nintcount = 0;
+    wai = 0;
+    inc = 0;
+    on = 1;
+end
+assign led[15:12] = extra[3:0];
+always @(posedge clk) begin
+    if(currentNum != 0) begin
+        wai <= 1;
+    end
+    if(wai == 1) begin
+        if(counter > 1000000) begin
+            //extra[3:0] <= check;
+            extra[3:0] <= buff[finalco][3:0];
+            finalco <= finalco + 1;
+            counter = 0;
+        end
+        if(nintcount == 96) begin
+            nintcount = 0;
+            counter <= counter + 1;
+            if(test == 1) begin
+                buff[bufco] = currentNum;
+                bufco <= bufco + 1;
+                if(bufco > 50000) begin
+                    test = 0;
+                end
+            end
+        end
+        nintcount <= nintcount + 1;
+    end 
+end
+
+//always @(posedge counter) begin
+    //if(counter > 100) begin
+        //extra[3:0] = currentNum[3:0];
+        
+        //extra[3:0] = buff[finalco][3:0];
+        //finalco = finalco + 1; 
+        
+//    end
+//end
+
+//always @(posedge bufco) begin
+//    buff[bufco] = currentNum;
+//end
+    
+endmodule
