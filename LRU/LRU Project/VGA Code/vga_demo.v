32ea07e11fce311163760ec7c28f4dacb1111586
aam08331 <112673088+aam08331@users.noreply.github.com>
2025-04-25 18:05:16 -0400

    Update vga_demo.v

--- LRU/LRU Project/VGA Code/vga_demo.v
+++ LRU/LRU Project/VGA Code/vga_demo.v
@@ -1,11 +1,11 @@
 module vga_demo
-   #(parameter CD = 12) // CD: Color Depth (12-bit RGB by default)
+   #(parameter CD = 12) 
    (
-    input clk,                       // Clock input
+    input clk,                       
     input wire endFile,
     // VGA output signals
-    output hsync, vsync,           // Horizontal and vertical sync
-    output [CD-1:0] rgb,            // RGB pixel output
+    output hsync, vsync,           
+    output [CD-1:0] rgb,            
     input wire [31:0] accessesTotal,
     input wire [31:0] evictionTotal,
     input wire [31:0] writeHitTotal,
@@ -17,14 +17,9 @@ module vga_demo
     input wire [31:0] missTotal
    );
 
-   // Internal wires for synchronization and RGB data
-   wire [10:0] hc, vc;                          // Horizontal and vertical counters (for pixel scanning)
-   wire [CD-1:0] vga_rgb;                       // Final RGB to VGA
+   wire [10:0] hc, vc;                          
+   wire [CD-1:0] vga_rgb;                       
 
-   // ------------------------ Assign background color and settings from switches
-
-   // ------------------------ Pattern Generator Instantiation
-   // Generates a colored square/bar based on current pixel (hc, vc)
    square_demo square_unit (
       .clk(clk),
       .endFile(endFile),
@@ -43,11 +38,10 @@ module vga_demo
    );
 
 
-   // ------------------------ VGA Sync Generator
-   // Generates pixel scan timing signals and outputs sync pulses
+
    vga_sync_demo #(.CD(CD)) sync_unit (
       .clk(clk), 
-      .reset(1'b0),                  // No external reset; hardcoded to 0
+      .reset(1'b0),                  
       .vga_si_rgb(vga_rgb),
       .hsync(hsync), 
       .vsync(vsync), 
