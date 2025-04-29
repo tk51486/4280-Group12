42e90aa49a230f4d172e1b03faf4e4ea11d38d69
tk51486 <95194163+tk51486@users.noreply.github.com>
2025-04-29 15:15:27 -0400

    Update IO_Management.v

--- LRU Final/SD Code/Our Code/IO_Management.v
+++ LRU Final/SD Code/Our Code/IO_Management.v
@@ -11,8 +11,19 @@
 module IO_Management (
     //General
     input wire clk,
-    //input wire start,
+    input wire start,
     output wire [15:0]  led, // 16 bit led
+    //Stats Tracked
+    input wire [31:0] accessesTotal,
+    input wire [31:0] evictionTotal,
+    input wire [31:0] writeHitTotal,
+    input wire [31:0] readHitTotal,
+    input wire [31:0] writeMissTotal,
+    input wire [31:0] readMissTotal,
+    //input wire [31:0] instTotal,
+    input wire [31:0] hitTotal,
+    input wire [31:0] missTotal,
+
     //Parsed Values
     output wire [16:0] LRUTag,
     output wire [10:0] LRUIndex,
@@ -26,21 +37,31 @@ module IO_Management (
     output wire         sdclk, // signals connect to SD bus
     inout               sdcmd,
     input  wire         sddat0,
-    output wire         uart_tx // UART tx signal, connected to host-PC's UART-RXD, baud=115200
+    output wire         uart_tx, // UART tx signal, connected to host-PC's UART-RXD, baud=115200
     //VGA    
-    
+    output hsync, vsync,           // Horizontal and vertical sync
+    output [11:0] rgb 
     
 );
+
+wire endFile;
+
+wire [31:0] instTotal;
 
 SD_Data_Decoder u_SD_Data_Decoder(
     .clk(clk),
     .led(led),
+    
+    .start(start),
     
     .LRUTag(LRUTag),
     .LRUIndex(LRUIndex),
     .LRULoadStore(LRULoadStore),
     .LRUInst(LRUInst),
     .LRULineReady(LRULineReady),
+    .instTotal(instTotal),
+    
+    .endFile(endFile),
     
     .clk_sd(clk_sd),
     .rstn(rstn),
@@ -51,9 +72,19 @@ SD_Data_Decoder u_SD_Data_Decoder(
     .uart_tx(uart_tx)
 );
 
-
-
-
-
-
-endmodule
+vga_demo u_vga_demo(
+    .clk(clk),
+    .hsync(hsync),
+    .vsync(vsync),
+    .rgb(rgb),
+    .endFile(endFile),
+    .instTotal(instTotal),
+    .accessesTotal(accessesTotal),
+    .evictionTotal(evictionTotal),
+    .writeHitTotal(writeHitTotal),
+    .readHitTotal(readHitTotal),
+    .writeMissTotal(writeMissTotal),
+    .readMissTotal(readMissTotal),
+    .hitTotal(hitTotal),
+    .missTotal(missTotal)
+);
