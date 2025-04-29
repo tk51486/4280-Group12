022dbbf51a43e810e20e5eb0ad0a2b09e3304cd2
tk51486 <95194163+tk51486@users.noreply.github.com>
2025-04-29 15:10:16 -0400

    Update DirectLRU.v

--- LRU Final/DirectLRU.v
+++ LRU Final/DirectLRU.v
@@ -1,20 +1,24 @@
 `timescale 1ns / 1ps
 
 module DirectLRU(
-    
+
+    //clock
     input clk,
-    
+
+    //each instruction, sent from SD to LRU
     input [16:0] LRUTag,
     input [10:0] LRUIndex,
     input LRULoadStore,
     input [20:0] LRUInst,
     input LRULineReady, 
 
+    //for RAM
     input clk_mem,
     input clk_cpu,
     input pll_locked,
     input CPU_RESETN,
-    
+
+    //LEDs, used as a "progress bar"
     output wire [14:0] led,
     
     //to stat tracker
@@ -45,19 +49,19 @@ module DirectLRU(
     output[1:0] ddr2_dm,
     output ddr2_odt
     );
-    
+
+    //to RAM
     reg [27:0] ram_addr, ram_addr_next;
     reg [63:0] line_to_ram;
+    reg ram_loadstore, ram_start;
+    //from RAM
     wire [63:0] line_from_ram;
     wire ram_transaction_complete, ram_ready;
-    reg ram_loadstore, ram_start;  //sent to ram 
-    
-    reg [15:0] debugLED;
-    //assign led[15:0] = debugLED;
+
+    //assigning progress bar
     assign led[14:0] = accessesTotal[20:6];
-    //assign led[7:0] = evictionTotal[31:24];
-    reg walloc;
-    
+
+    //creating mem controller
     MemController u_MemController(
         .clk_mem(clk_mem),
         .clk_cpu(clk_cpu),
@@ -86,7 +90,8 @@ module DirectLRU(
         .ddr2_dm(ddr2_dm),
         .ddr2_odt(ddr2_odt)
     );
-    
+
+    //definig stats and state registers
     localparam LRU_INIT = 3'h0;
     localparam LRU_IDLE = 3'h1;
     localparam LRU_READRAM = 3'h2;
@@ -94,7 +99,8 @@ module DirectLRU(
     localparam LRU_UPDATERAM = 3'h4;
     localparam LRU_WAIT = 3'h5;
     reg [2:0] lru_state, next_state, prev_state;
-    reg initializing, init_next; //used to initialize RAM
+    //used to initialize RAM
+    reg initializing, init_next; 
     
     initial begin
         ram_loadstore = 0;
@@ -105,13 +111,13 @@ module DirectLRU(
         debugLED = 16'h0000;
         initializing = 1;
         init_next = 1;
-        walloc = 1;
         evictionTotal = 0;
     end
-    
+
+    //used to detect when stats should be incremented, only happens once per instruction
     wire increment = (prev_state != LRU_PROCESS && lru_state == LRU_PROCESS);
     
-    //state register
+    //state register, assigning flops
     always @(posedge clk) begin
         prev_state <= lru_state;
         lru_state <= next_state;
@@ -119,17 +125,17 @@ module DirectLRU(
         initializing <= init_next;
     end
    
-   //counter logic
+   //counter logic, increments stats
    always @(posedge clk) begin
-        if (increment) begin
-            accessesTotal <= accessesTotal + 1;
+       if (increment) begin //checks flag
+            accessesTotal <= accessesTotal + 1; //increments every instruction
             if (~line_from_ram[63] || LRUTag != line_from_ram[61:45]) begin //miss, valid = 0 || no tag match
                 if (line_from_ram[62]) begin //dirty eviction
-                    evictionTotal <= evictionTotal + 1;
+                    evictionTotal <= evictionTotal + 1; //miss & dirty leads to eviction
                 end
-                if(LRULoadStore) begin   //write miss
+                if(LRULoadStore) begin //write miss
                     writeMissTotal <= writeMissTotal + 1;
-                end else begin  //read miss
+                end else begin //read miss
                     readMissTotal <= readMissTotal + 1;
                 end
                 missTotal <= missTotal + 1;
@@ -146,43 +152,42 @@ module DirectLRU(
     
     //next-state logic
     always @(*) begin
-        //debugLED[15:13] = lru_state;
+        //set LED values
         debugLED = evictionTotal[15:0];
         
-        
+        //default values for flops
         next_state = lru_state;
         ram_addr_next = ram_addr;
         init_next = initializing;
         
         case(lru_state)
-            LRU_INIT: begin
-                if (ram_ready) begin
-                    if (initializing == 0) begin //initialization done
+            LRU_INIT: begin //initialize RAM by setting all relevant addresses to 64'b0
+                if (ram_ready) begin //waits until RAM is ready internally
+                    if (initializing == 0) begin //if initialization done,
                         next_state = LRU_READRAM;  //wait for first cache instruction
-                    end else begin
+                    end else begin //otherwise,
                         next_state = LRU_UPDATERAM; //clear current address
                     end
                 end
             end
-            LRU_IDLE: begin
-                //debugLED = line_from_ram[63:48];
-                if (LRULineReady) begin
-                    ram_addr_next = {LRUIndex, 17'b0};
-                    next_state = LRU_READRAM;
+            LRU_IDLE: begin //waits for each instruction to be parsed
+                if (LRULineReady) begin //wait for ready flag from SD
+                    ram_addr_next = {LRUIndex, 17'b0}; //change address to read from
+                    next_state = LRU_READRAM; //read RAM
                 end
             end
-            LRU_READRAM: begin
+            LRU_READRAM: begin //sets flag to make MemController read from RAM 
                 if (~ram_transaction_complete) begin //start read
                     ram_start = 1;
                     ram_loadstore = 0;
-                end else begin
+                end else begin //wait for transaction flag
                     ram_start = 0;
-                    next_state = LRU_PROCESS;  //wait for read to finish
+                    next_state = LRU_PROCESS;  //begin handling line
                 end
             end
-            LRU_PROCESS: begin
+            LRU_PROCESS: begin //updates RAM if necessary
                 if (~ram_transaction_complete) begin //wait until flag is cleared
-                    next_state = LRU_WAIT;
+                    next_state = LRU_WAIT; //default next state
                     
                     if (~line_from_ram[63] || LRUTag != line_from_ram[61:45]) begin //miss
                         if (line_from_ram[62]) begin //updating cache, maintain dirty
@@ -190,36 +195,36 @@ module DirectLRU(
                         end else begin
                             line_to_ram[31:13] = {1'b1, line_from_ram[62], LRUTag};
                         end
-                        next_state = LRU_UPDATERAM;
+                        next_state = LRU_UPDATERAM; //write to RAM if needed
                     end
                     if (LRULoadStore) begin //setting dirty on write
                         line_to_ram[30] = 1;
-                        next_state = LRU_UPDATERAM;
+                        next_state = LRU_UPDATERAM; //write to RAM if needed
                     end
                 end
             end
-            LRU_UPDATERAM: begin
+            LRU_UPDATERAM: begin //sets flag to make MemController write to RAM
                 if (~ram_transaction_complete ) begin //start write
                     ram_start = 1;
                     ram_loadstore = 1;
-                end else begin
+                end else begin //wait for transaction flag
                     ram_start = 0;
-                    next_state = LRU_WAIT;  //wait for write to finish
+                    next_state = LRU_WAIT;  //proceed to WAIT. necessary for initialization
                 end
             end
-            LRU_WAIT: begin
-                if (initializing) begin 
+            LRU_WAIT: begin //handle initialization, proceed to next state
+                if (initializing) begin
                     if (ram_addr[27:17] == 2047) begin    //RAM has been cleared
-                        init_next = 0;
+                        init_next = 0; //stop initializing
                     end else begin  //if not, increment address
-                        ram_addr_next = ram_addr + {11'b1, 17'b0};
+                        ram_addr_next = ram_addr + {11'b1, 17'b0}; //only using 2048 (2^11) addresses
                     end
-                    next_state = LRU_INIT;
+                    next_state = LRU_INIT; //INIT will handle the next state
                 end else begin //just updated cache
-                    next_state = LRU_IDLE;
+                    next_state = LRU_IDLE; //just begin waiting
                 end
             end
-            default: next_state = LRU_INIT;
+            default: next_state = LRU_INIT; //default state
     endcase
     end
     
